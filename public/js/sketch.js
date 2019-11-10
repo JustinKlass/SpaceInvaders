f8c67969c4fb434b2580b7e52e1be2eb55292c65
Justin Klass <Justin@Justins-MacBook.home>
2019-11-09 22:29:24 -0500

    Add functions to clean up code. Left in old code commented out just in case I need to go back and ref

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -7,8 +7,57 @@ let alienImage;
 let pews = [];
 let aliens = [];
 let aliens2 = [];
+let aliensArray = [];
 let backgroundImage;
 
+
+const shift = (aliensArray) => {
+    
+    for (let i = 0; i < aliensArray.length; i++) {
+        for (let j = 0; j < aliensArray[i].length; j++) {
+            aliensArray[i][j].shiftDown();
+        }
+    }
+};
+
+const setupAlienRow = (alienRowName, y) => {
+
+    for (let i = 0; i < 8; i++) {
+        alienRowName[i] = new Alien(i * 125, y);
+    }
+    aliensArray.push(alienRowName);
+    console.log(aliensArray);
+
+};
+
+const makeAlienRow = (alienRowName) => {
+
+
+    for (let i = 0; i < alienRowName.length; i++) {
+        alienRowName[i].show();
+        alienRowName[i].move();
+
+    }
+
+    // ALIEN SET 1 SCORE AND HIT DETECTION
+    for (let i = alienRowName.length - 1; i >= 0; i--) {   //Start looping through Array backwards so that it doesn't skip backwards
+        if (alienRowName[i].demo) {
+            score = score + 100;
+            alienRowName.splice(i, 1);
+        }
+    }
+
+
+// MAKING FUNCTIONS SO THAT EVERYTHING LOOKS NOICE AND FANCY
+// MULTIPLE DRAW FUNCTIONS IN AN IF STATEMENT? LOOK UP P5 VIDS
+// ADD EXTRA CANVAS THAT GOES OVER FIRST AND HAS A PLAY AND SCORES 
+    // BUTTON GOES AWAY WHEN YOU HIT PLAY OR SCORE
+// MAKE WIN/LOSS LOGIC AND HAVE IT REDIRECT TO ENTER NEW SOCRE OR PLAY AGAIN
+
+
+
+
+};
 
 function preload() {
 
@@ -18,26 +67,30 @@ function preload() {
 }
 
 function setup() {
-
     createCanvas(1260, 625);
     ship = new Ship();
 
 
     // ALIEN SET 1
-    for (let i = 0; i < 8; i++) {
-        aliens[i] = new Alien(i * 125, 50);
-    }
+
+    setupAlienRow(aliens, 50);
+
+    // for (let i = 0; i < 8; i++) {
+    //     aliens[i] = new Alien(i * 125, 50);
+    // }
 
     // ALIEN SET 2
-    for (let i = 0; i < 8; i++) {
-        aliens2[i] = new Alien(i * 125, 110);
-    }
+
+    setupAlienRow(aliens2, 110);
+
+    // for (let i = 0; i < 8; i++) {
+    //     aliens2[i] = new Alien(i * 125, 110);
+    // }
 }
 
 function draw() {
-    
+
     let edge = false;
-    let rowTwoEdge = false;
 
     background(backgroundImage, 255);
 
@@ -48,6 +101,9 @@ function draw() {
     ship.show();
     ship.move();
 
+    makeAlienRow(aliens);
+    makeAlienRow(aliens2);
+    
 
     // HIT DETECTION
     for (let i = 0; i < pews.length; i++) {
@@ -71,58 +127,78 @@ function draw() {
     }
 
 
-    // ALIEN SET 1 MOVE AND SHOW
-    for (let i = 0; i < aliens.length; i++) {
-        aliens[i].show();
-        aliens[i].move();
+    // // ALIEN SET 1 MOVE AND SHOW
+    // for (let i = 0; i < aliens.length; i++) {
+    //     aliens[i].show();
+    //     aliens[i].move();
 
+    //     if (aliens[i].x >= width || aliens[i].x <= 0) {
+    //         edge = true;
+    //     }
+    // }
+
+    // // ALIEN SET 1 SCORE AND HIT DETECTION
+    // for (let i = aliens.length - 1; i >= 0; i--) {   //Start looping through Array backwards so that it doesn't skip backwards
+    //     if (aliens[i].demo) {
+    //         score = score + 100;
+    //         aliens.splice(i, 1);
+    //     }
+    // }
+
+
+
+
+
+
+
+    // // ALIEN SET 2 MOVE AND SHOW
+    // for (let i = 0; i < aliens2.length; i++) {
+    //     aliens2[i].show();
+    //     aliens2[i].move();
+
+    //     if (aliens2[i].x >= width || aliens2[i].x <= 0) {
+    //         edge = true;
+    //     }
+    // }
+
+    // // ALIEN SET 2 SCORE AND HIT DETECTION
+    // for (let i = aliens2.length - 1; i >= 0; i--) {   //Start looping through Array backwards so that it doesn't skip backwards
+    //     if (aliens2[i].demo) {
+    //         score = score + 100;
+    //         aliens2.splice(i, 1);
+    //     }
+    // }
+
+
+
+    for (let i = 0; i < aliens.length; i++) {
         if (aliens[i].x >= width || aliens[i].x <= 0) {
             edge = true;
         }
     }
 
-    // ALIEN SET 1 SCORE AND HIT DETECTION
-    for (let i = aliens.length - 1; i >= 0; i--) {   //Start looping through Array backwards so that it doesn't skip backwards
-        if (aliens[i].demo) {
-            score = score + 100;
-            aliens.splice(i, 1);
-        }
-    }
-
-
-
-
-
-
-
-
-
-    // ALIEN SET 2 MOVE AND SHOW
     for (let i = 0; i < aliens2.length; i++) {
-        aliens2[i].show();
-        aliens2[i].move();
-
         if (aliens2[i].x >= width || aliens2[i].x <= 0) {
             edge = true;
         }
     }
 
-    // ALIEN SET 2 SCORE AND HIT DETECTION
-    for (let i = aliens2.length - 1; i >= 0; i--) {   //Start looping through Array backwards so that it doesn't skip backwards
-        if (aliens2[i].demo) {
-            score = score + 100;
-            aliens2.splice(i, 1);
-        }
+    if (edge === true) {
+        // for (let i = 0; i < aliens.length; i++) {
+        //     aliens[i].shiftDown();
+        //     shift(aliensArray);
+        // }
+        // for (let i = 0; i < aliens2.length; i++) {
+        //     aliens2[i].shiftDown();
+        //     shift(aliensArray);
+        // }
+        shift(aliensArray)
     }
 
-    if (edge === true) {
-        for (let i = 0; i < aliens.length; i++) {
-            aliens[i].shiftDown();
-        }
-        for (let i = 0; i < aliens2.length; i++) {
-            aliens2[i].shiftDown();
-        }
-    }
+
+
+
+
 
 
 
